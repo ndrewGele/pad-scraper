82ba7ff950743f7c5d2411ee777fcf6c68e217c0
ndrewGele <andrew.gele@gmail.com>
2019-05-10 02:14:04 -0400

    Spider does a better job of scraping skills now

--- pad_scraper/pad_scraper/spiders/monster_spider.py
+++ pad_scraper/pad_scraper/spiders/monster_spider.py
@@ -4,9 +4,11 @@ from re import sub
 from datetime import datetime
 
 
+# Following Scrapy Tutorial Mostly
 class MonsterSpider(Spider):
+
     name = 'monster'
-    
+
     def __init__(self, start = '1', end = '5', **kwargs):
 
         super().__init__(**kwargs)
@@ -19,16 +21,19 @@ class MonsterSpider(Spider):
     def parse(self, response):
         sel = Selector(response)
         
+        
         # Get Profile Table
         profile = sel.xpath("//table[@class='tableprofile']//td[@class='data']")
         limit = 1
         if len(profile) >= 6:
             limit = profile[5].xpath('normalize-space(./text())').get()
             if limit == '--': limit = '1.0' 
+        
 
         # Get Stats Table
         stats = sel.xpath("//div[@id='comparechart']//tr")
-        
+                
+
         # Get Awakenings and S.Awakenings if they exist
         awoken = sel.xpath("//td[@class='awoken1']")
         awakenings = []
@@ -38,9 +43,19 @@ class MonsterSpider(Spider):
         if len(awoken) > 1:
             s_awakenings = [a[-2:] for a in awoken[1].css('a::attr(href)').getall()]
 
+        
         # Get Skill Text
-        skills = sel.xpath("//table[@id='tablestat']//td[@class='value-end']/text()").getall()
+        active = sel.xpath("//tr[td='Active Skill:']/following-sibling::tr[1][td='Effects:']/td[@class='value-end']//text()").getall()        
+        if len(active) == 0: active = 'None' 
+        else: active = ''.join(active)
 
+        lead = sel.xpath("//tr[td='Leader Skill:']/following-sibling::tr[1][td='Effects:']/td[@class='value-end']//text()").getall()
+        if len(lead) == 0: lead = 'None' 
+        else: lead = ''.join(lead)
+        
+
+
+        # Return this nice json
         yield {
             'NUMBER': response.url[46:],
             'NAME': sel.css('h1::text').get(),
@@ -55,7 +70,7 @@ class MonsterSpider(Spider):
             'WGHT': stats[5].css('td::text').getall()[1],
             'AWAKE': [sub("=", "", x) for x in awakenings],
             'S_AWAKE': [sub("=", "", x) for x in s_awakenings],
-            'SKILL_TEXT': skills[0],
-            'LEAD_TEXT': sub("\r\n\t\t\t\t\t\t", "None", skills[4]), # Maybe there's a better way to deal with this garbage lmao
+            'SKILL_TEXT': active,
+            'LEAD_TEXT': lead,
             'READ_DATE': datetime.now()
         }
