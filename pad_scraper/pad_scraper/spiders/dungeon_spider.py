976c1fff1c8fd6867d20818553f67ebe3214fcd8
ndrewGele <andrew.gele@gmail.com>
2019-06-17 01:05:23 -0400

    Some progress on dungeon spider.

--- pad_scraper/pad_scraper/spiders/dungeon_spider.py
+++ pad_scraper/pad_scraper/spiders/dungeon_spider.py
@@ -20,28 +20,42 @@ class DungeonSpider(Spider):
 
     def parse(self, response):
         sel = Selector(response)
-        
-        
+
+
 
         # Basic Dungeon Info
-        sections = sel.xpath("//div[@id='content']/table//td[@class='section']").getall()
-        section_names = sel.xpath("//div[@id='content']/table//h2/text()").getall()
-        print(section_names)
+        sections = sel.xpath("//div[@id='content']/table//td[@class='section']")
+        sections[0].xpath("table//tr[2]//td[2]/text()").get()  #table/tbody/tr[1]/td/text()
 
 
+        # Create Nested List of Floors
+        floors = []
+        for floor in sel.xpath("//tr[td[@class='enemy']]"):
+            
+            
+            skills = []
+            for skill in floor.xpath("FILLTHISIN"):
+                skills.append({
+                    'SKILL_NAME': skill.xpath(""),
+                    'SKILL_TEXT': skill.xpath("") 
+                })
 
-        # Get Awakenings and S.Awakenings if they exist
-        nums = sel.xpath("//td[@class='awokenindex']//a/@href").getall()
-        descs = sel.xpath("//td[@class='awokenindex']//img/@title").getall()
-
-
+            floors.append({
+                'FLOOR': floor.xpath("td[1]/text()"),
+                'MONSTER': 'mon',
+                'TURN': 'trn',
+                'DEFENSE': 'def',
+                'HP': 'hp',
+                'SKILLS': skills
+            })
 
         # Return this nice json
         yield {
             'DUNGEON_NUM': response.url[46:],
-            'DUNGEON_NAME': 'monster come get u',
-            'DUNGEON_TYPE': 'tech/special/normal',
-            'SUB_DUNGEON': 'big monster - mythical',
-            'FLOORS': ['nested', 'list', 'of', 'encounters'],
+            'DUNGEON_NAME': sub(r'\W+', ' ', sections[1].xpath("table//tr[2]/td/text()").get()).strip(),
+            'DUNGEON_TYPE': sub(r'\W+', ' ', sections[1].xpath("table//tr[4]/td/text()").get()).strip(),
+            'SUB_DUNGEON': sections[0].xpath("table//tr[2]//td[2]/text()").get(),
+            'STAMINA': sections[0].xpath("table//tr[4]//td[2]/span/text()").get(),
+            'FLOORS': floors,
             'READ_DATE': datetime.now()
         }
